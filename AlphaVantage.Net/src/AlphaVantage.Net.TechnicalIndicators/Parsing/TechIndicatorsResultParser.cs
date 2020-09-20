2cda2bff177a4a3866788f02a396afb958c68066
Kirill <lucenko_kirill@mail.ru>
2020-09-20 11:09:08 +0300

    technical indicators package

--- AlphaVantage.Net/src/AlphaVantage.Net.TechnicalIndicators/Parsing/TechIndicatorsResultParser.cs
+++ AlphaVantage.Net/src/AlphaVantage.Net.TechnicalIndicators/Parsing/TechIndicatorsResultParser.cs
@@ -1,13 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
+using AlphaVantage.Net.Core.Exceptions;
 using AlphaVantage.Net.Core.Parsing;
 
 namespace AlphaVantage.Net.TechnicalIndicators.Parsing
 {
-    public class TechIndicatorsResultParser : IAlphaVantageJsonDocumentParser<TechIndicatorResult>
+    internal class TechIndicatorsResultParser : IAlphaVantageJsonDocumentParser<TechIndicatorResult>
     {
         public TechIndicatorResult ParseApiResponse(JsonDocument jsonDocument)
         {
-            throw new System.NotImplementedException();
+            var result = new TechIndicatorResult();
+            
+            try
+            {
+                result.MetaData = jsonDocument.ExtractMetaData();
+                result.DataPoints = GetDataPoints(jsonDocument);
+                
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new AlphaVantageParsingException(
+                    "Error occured while parsing Technical Indicators response",
+                    ex);
+            }
+        }
+
+        private static List<TechIndicatorDataPoint> GetDataPoints(JsonDocument jsonDocument)
+        {
+            var result = new List<TechIndicatorDataPoint>();
+
+            var dataPointsJsonElement = jsonDocument.RootElement.EnumerateObject().Last().Value;
+
+            foreach (var dataPointJson in dataPointsJsonElement.EnumerateObject())
+            {
+                var dataPointFieldsJson = dataPointJson.Value;
+
+                var dataPoint = new TechIndicatorDataPoint
+                {
+                    Time = dataPointJson.Name.ParseToDateTime(),
+                    Parameters = GetParameters(dataPointsJsonElement)
+                };
+                
+                result.Add(dataPoint);
+            }
+
+            return result;
+        }
+
+        private static List<TechIndicatorParameter> GetParameters(JsonElement dataPointFieldsJson)
+        {
+            var result = new List<TechIndicatorParameter>();
+            
+            foreach (var fieldJson in dataPointFieldsJson.EnumerateObject())
+            {
+                var techIndicatorParameter = new TechIndicatorParameter()
+                {
+                    ParameterName = fieldJson.Name,
+                    ParameterValue = fieldJson.Value.GetString().ParseToDecimal()
+                };
+                
+                result.Add(techIndicatorParameter);
+            }
+
+            return result;
         }
     }
 }
